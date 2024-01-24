e94a5b46536f3aea4f17d1ea9c9b4dce0ccfb7a6
Rabea Gleissner <rabeagleissner@gmail.com>
2024-01-24 20:30:06 +0700

    Add favicon

--- lib/connect_game_web/endpoint.ex
+++ lib/connect_game_web/endpoint.ex
@@ -20,7 +20,7 @@ defmodule ConnectGameWeb.Endpoint do
     at: "/",
     from: :connect_game,
     gzip: false,
-    only: ~w(assets fonts images favicon.ico robots.txt)
+    only: ~w(assets fonts images favicon.ico robots.txt manifest.json)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
